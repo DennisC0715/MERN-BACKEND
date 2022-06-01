b1840b84427b44f351f8be125cfadc06a0f38cb7
Dennis Chen <chensuyuan1989@hotmail.com>
2022-05-31 21:57:40 -0400

    remove multiple value

--- app.js
+++ app.js
@@ -22,11 +22,10 @@ const authenticate = require("./middleware/authenticate");
 // app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", [
-    "https://mern-app-project-frontend.herokuapp.com",
-    "https://mern-web-application.vercel.app/",
-    "http://localhost:3000/",
-  ]);
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://mern-app-project-frontend.herokuapp.com"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested, Content-Type, Accept Authorization"
