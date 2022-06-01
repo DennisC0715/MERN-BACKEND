a103bd92172504ce190fae4fb0745040f8a0124a
Dennis Chen <chensuyuan1989@hotmail.com>
2022-05-31 21:55:02 -0400

    remove* add url

--- app.js
+++ app.js
@@ -22,7 +22,11 @@ const authenticate = require("./middleware/authenticate");
 // app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", [
+    "https://mern-app-project-frontend.herokuapp.com",
+    "https://mern-web-application.vercel.app/",
+    "http://localhost:3000/",
+  ]);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested, Content-Type, Accept Authorization"
