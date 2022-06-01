0f0ee1323f9cb9c247ddb1ecfe9b71e0aa089ba5
Dennis Chen <chensuyuan1989@hotmail.com>
2022-05-31 22:08:32 -0400

    push cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
@@ -18,25 +18,9 @@ const Users = require("./models/userSchema");
 const Message = require("./models/msgSchema");
 const authenticate = require("./middleware/authenticate");
 
+app.use(cors({ origin: process.env.URL, credentials: true }));
+
 //These Method is Used to Get Data and cookie from frontEnd
-// app.use(cors());
-
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://mern-app-project-frontend.herokuapp.com"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
